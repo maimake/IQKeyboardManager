cc5ef00bfcba1063be72053cc3f39b9a50fc3b00
mai <yshxinjian@gmail.com>
2016-01-01 14:45:51 +0800

    fixed:wrong contentsize when contentSize.height<frame.size.height

--- IQKeyBoardManager/IQKeyboardManager.m
+++ IQKeyBoardManager/IQKeyboardManager.m
@@ -743,13 +743,14 @@ void _IQShowLog(NSString *logString);
 
                 } completion:NULL];
 
-                //Maintaining contentSize
-                if (_lastScrollView.contentSize.height<_lastScrollView.frame.size.height)
-                {
-                    CGSize contentSize = _lastScrollView.contentSize;
-                    contentSize.height = _lastScrollView.frame.size.height;
-                    _lastScrollView.contentSize = contentSize;
-                }
+                //wrong content size when a tableview embbed in ViewController.view
+//                //Maintaining contentSize
+//                if (_lastScrollView.contentSize.height<_lastScrollView.frame.size.height)
+//                {
+//                    CGSize contentSize = _lastScrollView.contentSize;
+//                    contentSize.height = _lastScrollView.frame.size.height;
+//                    _lastScrollView.contentSize = contentSize;
+//                }
                 
                 _IQShowLog([NSString stringWithFormat:@"%@ new ContentInset : %@",[_lastScrollView _IQDescription], NSStringFromUIEdgeInsets(_lastScrollView.contentInset)]);
             }
